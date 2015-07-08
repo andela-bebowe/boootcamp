3c74d8fcb0cab4b3bf5169d57284cf85e19fef62
blessinge <blessingebowe@gmail.com>
2015-07-08 08:57:32 +0100

    Had to include comments to increase the line of code

--- problem3.py
+++ problem3.py
@@ -1,8 +1,6 @@
-
 def search_file(file_txt):
 	retain = ""
 	my_file = open(file_txt, "r")
-	
 	line_number = 1
 	count = 0
 	total_count = 0
@@ -13,15 +11,19 @@ def search_file(file_txt):
 		no = 0
 		retain = my_file.readline()
 		for check in retain:
+			#checks number of uppercase letters
 			if check.isupper():
 				no_upper += 1
+			#checks number of lowercase letters
 			elif check.islower():
 				no_lower += 1
+			#checks number of numbers
 			elif check.isdigit():
 				no += 1
-		#the conditional below can be changed depending on what u want
+		#the conditional below can be changed depending on what u want to display
 		if no_upper >= 4 and no_lower >= 2 and no >= 4:
 			count += 1
 		line_number += 1
+	#return the number of counts after the while loop	
 	return count
 print search_file("password.txt")
