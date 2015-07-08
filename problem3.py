7ebccbc313062db63df00c2bf2066caedae0dabe
blessinge <blessingebowe@gmail.com>
2015-07-08 07:54:29 +0100

    Have corrected this code to run properly

--- problem3.py
+++ problem3.py
@@ -12,18 +12,16 @@ def search_file(file_txt):
 		no_lower = 0
 		no = 0
 		retain = my_file.readline()
-		retain_list = list(retain)
 		for check in retain:
 			if check.isupper():
 				no_upper += 1
 			elif check.islower():
 				no_lower += 1
-			else:
+			elif check.isdigit():
 				no += 1
 		#the conditional below can be changed depending on what u want
-		if no_upper >= 4 and no_lower >= 2 and no == 5:
+		if no_upper >= 4 and no_lower >= 2 and no >= 4:
 			count += 1
-			print retain
 		line_number += 1
 	return count
 print search_file("password.txt")
